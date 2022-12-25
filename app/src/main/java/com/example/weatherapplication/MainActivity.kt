de3843b4b5be989ba11829eb62304db9ecb3aed1
SajalRuhela017 <ruhela.sajal017@gmail.com>
2022-12-25 09:44:00 +0530

    Initial Commit

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -22,78 +22,8 @@ import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 //https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=d782c3641dcc9cbcb8528dfb0245b3e4
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mfusedlocation:FusedLocationProviderClient
-    private var myRequestCode = 6969
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getLastLocation() {
-        if(CheckPermission()) {
-            if(LocationEnable()) {
-                mfusedlocation.lastLocation.addOnCompleteListener{
-                    task -> var location:Location?=task.result
-                    if(location == null) {
-                        NewLocation()
-                    }else{
-                        Log.i("Location" , location.longitude.toString())
-                    }
-                }
-            }else{
-                Toast.makeText(this, "Please enable location services", Toast.LENGTH_SHORT).show()
-            }
-        }else{
-            RequestPermission()
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun NewLocation() {
-        var locationRequest = com.google.android.gms.location.LocationRequest()
-        locationRequest.priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 1
-        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
-        mfusedlocation.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
-
-    }
-
-    private val locationCallback=object:LocationCallback() {
-        override fun onLocationResult(p0: LocationResult) {
-           var lastLocation: Location? =p0.lastLocation
-        }
-    }
-
-    private fun LocationEnable(): Boolean {
-        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
-
-    private fun RequestPermission() {
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            android.Manifest.permission.ACCESS_FINE_LOCATION),myRequestCode)
-    }
-
-    private fun CheckPermission(): Boolean {
-        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    @SuppressLint("MissingSuperCall")
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        if(requestCode == myRequestCode) {
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getLastLocation()
-            }
-        }
     }
 }
